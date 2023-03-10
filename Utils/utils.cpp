827af3a789ecd9e5d241c4fc38e3a117f2c86b17
MihaiMocanuGit <mihai.mocanu.0220@gmail.com>
2023-03-10 13:09:57 +0200

    added 2 utils functions

--- Utils/utils.cpp
+++ Utils/utils.cpp
@@ -7,3 +7,13 @@ int Utils::powInt(int base, int power)
         result *= base;
     return result;
 }
+
+int Utils::getElementAtIJ(int i, int j, int arr[], int numberColumns)
+{
+    return arr[i * numberColumns + j];
+}
+
+int Utils::setElementAtIJ(int i, int j, int value, int arr[], int numberColumns)
+{
+    arr[i * numberColumns + j] = value;
+}
--- Utils/utils.h
+++ Utils/utils.h
@@ -5,6 +5,8 @@ namespace Utils
 {
     int powInt(int base, int power);
 
+    int getElementAtIJ(int i, int j, int arr[], int numberColumns);
+    int setElementAtIJ(int i, int j, int value, int arr[], int numberColumns);
 }
 
 #endif //UTILS_H
