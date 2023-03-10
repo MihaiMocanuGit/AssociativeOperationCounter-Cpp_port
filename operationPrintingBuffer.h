c926648f71980c8e9eea8c8f7cb3bc840d88dea2
MihaiMocanuGit <mihai.mocanu.0220@gmail.com>
2023-03-10 15:23:41 +0200

    implemented a printer class specialized on printing operatations to files

---
+++ operationPrintingBuffer.h
@@ -0,0 +1,66 @@
+#ifndef OPERATION_PRINTING_BUFFER_H
+#define OPERATION_PRINTING_BUFFER_H
+
+#include <fstream>
+#include <string>
+
+//#include <queue>
+
+#include "Utils/utils.h"
+
+template <std::size_t CARDINAL_SET>
+class OperationPrintingBuffer
+{
+private:
+    std::string outputFile = "output_";
+    std::ofstream m_outputStream;
+
+    static constexpr int CARDINAL_SET_SQ = CARDINAL_SET * CARDINAL_SET;
+
+    int m_totalNoOfPrints = 0;
+
+    //std::queue<> operationBuffer
+public:
+    OperationPrintingBuffer()
+    {
+        outputFile += std::to_string(CARDINAL_SET));
+        outputFile += ".txt";
+
+        m_outputStream.open(outputFile );
+    }
+    
+    //void pushToBuffer(const int operationArray[CARDINAL_SET_SQ]);
+    //void flushBuffer();
+
+    void print(const int operationArray[CARDINAL_SET_SQ])
+    {
+        for (int i = 0; i < CARDINAL_SET; ++i)
+        {
+            for (int j = 0; j < CARDINAL_SET; ++j)
+            {
+                int element = Utils::getElementAtIJ(i,h, operationArray, CARDINAL_SET);
+                element += 1;
+
+                std::string completeElement = "a";
+                completeElement += std::to_string(element);
+
+                m_outputStream << completeElement << ' ';
+            }
+            m_outputStream << '\n';
+        }
+
+        m_totalNoOfPrints++;
+        m_outputStream << "\t\t\t" << m_totalNoOfPrints << "\n\n";
+
+    }
+
+    ~OperationPrintingBuffer()
+    {
+        m_outputStream.close();
+    }
+
+
+};
+
+
+#endif //GROUP_PRINTINOPERATION_PRINTING_BUFFER_HG_BUFFER_H
